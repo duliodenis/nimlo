4dca7cdc60516121dc12e9fbe8b12905acd64058
Dulio Denis <dulio.denis@yahoo.com>
2026-06-14 14:15:30 -0400

    Bump version to 0.6

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,7 @@ const app = @import("app/app.zig");
 
 pub fn main() !void {
     std.debug.print("Nimlo starting...\n", .{});
-    std.debug.print("Nimlo MVP milestone 0.5\n", .{});
+    std.debug.print("Nimlo MVP milestone 0.6\n", .{});
 
     try app.run();
 }
